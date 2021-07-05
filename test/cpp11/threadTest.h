8ad571871442bd32c5158fbfde978bb912857305
Mr-liu-2021 <1937380272@qq.com>
2021-07-05 20:41:14 +0800

    three submit

--- test/cpp11/threadTest.h
+++ test/cpp11/threadTest.h
@@ -12,20 +12,19 @@
 
 #include <Logger.hpp>
 void fun1(){
-  std::shared_ptr<Logger> logger = std::make_shared<Logger>("ThreadTest::fun1()");
+  std::shared_ptr<Logger> logger = std::make_shared<Logger>("ThreadTest::fun1()", Logger::file_and_terminal, "./log/thread.log");
   logger->Info("fun1() start...");
 
   logger->Info("fun1() end...");
 }
 
 void test7() {
-  std::shared_ptr<Logger> logger = std::make_shared<Logger>("ThreadTest::test7()");
+  std::shared_ptr<Logger> logger = std::make_shared<Logger>("ThreadTest::test7()", Logger::file_and_terminal, "../log/thread.log");
 
   std::thread th_1(fun1);
   th_1.detach();
 
   logger->Info("hello world...");
-  logger->Info("xxx");
 
 }
 
